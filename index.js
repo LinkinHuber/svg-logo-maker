90304d6df22c3220b0d0bf3fe992448bf77342b6
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-05-01 12:36:48 -0500

    got shapes to generate along with the text and the colors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const { writeFile } = require('fs').promises;
+const fs = require('fs');
 const {Square, Circle, Triangle} = require('./lib/shapes.js');
 
 
@@ -13,7 +13,7 @@ const gatherInfo = () => {
         if (your_title.length === 3) {
           return true;
         } else {
-          console.log("Text must be exactly 3 characters long")
+          console.log('Text must be exactly 3 characters long')
           return false;
         }
        }
@@ -21,7 +21,7 @@ const gatherInfo = () => {
     {
       type: 'input',
       name: 'textColor',
-      message: 'What color do you want the text to be?',
+      message: 'Enter a color or hexidecimal number for the text',
     },
     {
       type: 'list',
@@ -32,65 +32,40 @@ const gatherInfo = () => {
     {
       type: 'input',
       name: 'shapeColor',
-      message: 'What color do you want the shape to be?',
+      message: 'Enter a color or hexidecimal number for the shape',
     },
   ])
 }
 
 
-// const fs = require('fs');
-// function createLogo() {
-// 	inquirer.prompt(questions)
-// 		.then(data => {
-// 			console.log(data)
-// 			let shape = ""
-// 			if (data.shape === "Circle") {
-// 				shape = new Circle()
+function createSVG() {
+	gatherInfo()
+		.then(data => {
+			let shape = ''
+      if (data.shape === 'Square') {
+				shape = new Square()
+      }
+      if (data.shape === 'Circle') {
+        shape = new Circle()
+			}
+			if (data.shape === 'Triangle') {
+				shape = new Triangle()
+			}
+    
+			shape.setColor(data.shapeColor)
 
-// 			}
-// 			if (data.shape === "Triangle") {
-// 				shape = new Triangle()
+			return `<svg version='1.1' width='300' height='200' xmlns='http://www.w3.org/2000/svg'> 
 
-// 			}
-
-// 			if (data.shape === "Square") {
-// 				shape = new Square()
-
-// 			}
-// 			shape.setColor(data.color)
-// 			console.log(shape)
-// 			return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-// 		${shape.render()
-// 				}
+		  ${shape.render()}
 	
-// 		<text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.logo}</text>
+		  <text x='150' y='125' font-size='60' text-anchor='middle' fill='${data.textColor}'>${data.text}</text>
 	
-// 	</svg>`
+	    </svg>`
 
-// 		})
-// 		.then(svg=>{
-// 			console.log(svg)
-// 			fs.writeFileSync("logo.svg", svg)
-// 		})
-// }
-// createLogo()
-
-
-const generateSVG = ({text, textColor}) => 
-`<svg version='1.1' width='300' height='200' xmlns='http://www.w3.org/2000/svg'>
-
- ${shape.render()}
-
- <text x='150' y='125' font-size='60' text-anchor='middle' fill='${textColor}'>${text}</text>
-
- </svg>`
-
-
-const init = () => {
-  gatherInfo()
-  .then((data) => writeFile('./examples/logo.svg', generateSVG(data)))
-  .then(() => console.log('Successfully generated logo.svg'))
-  .catch((err) => console.error(err));
-};
-
-init();
+		})
+		.then(svg=>{
+			fs.writeFileSync('./examples/logo.svg', svg)
+      console.log('Generated logo.svg')
+		})
+}
+createSVG()
